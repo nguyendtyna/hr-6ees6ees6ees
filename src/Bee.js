6a88a49441212f9ea807286a9d6e43ea3b4714a5
Tyna Nguyen <ngtyna@gmail.com>
2020-10-12 12:30:59 -0500

    Implement a Bee subclass to inherit properties from Grub (ES6)

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,19 @@
-class Bee {
-  // TODO..
+/*
+
+Bee = Subclass of Grub (superclass)
+Implement in ES6 style (use class, extends, super keywords)
+
+*/
+
+class Bee extends Grub {
+  constructor() {
+    // Override the constructor, pass args that base class needs
+    super ();
+
+    this.age = 5;
+    this.color = 'yellow';
+
+    // Add new Bee properties
+    this.job = 'Keep on growing';
+  }
 };
