39075017391754bdf3b79ac0efab9b2ac143129a
Tyna Nguyen <ngtyna@gmail.com>
2020-10-12 16:29:53 -0500

    Implement a RetiredForagerBee subclass to inherit properties from ForagerBee (ES6)

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,28 @@
-class RetiredForagerBee {
-  // TODO..
+/*
+
+RetiredForagerBee = Subclass of ForagerBee (superclass)
+Implement in ES6 style (use class, extends, super keywords)
+
+*/
+
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    // Calls the ForagerBee superclass
+    super();
+
+    this.age = 40;
+    this.job = 'gamble';
+    this.color = 'grey'
+    this.canFly = false;
+    // treasureChest property in
+  }
+
+  // Add new RetiredForagerBee methods
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
