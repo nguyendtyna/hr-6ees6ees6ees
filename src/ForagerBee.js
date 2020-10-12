9cb3179b05a1cc5e9f27b7df6539b1c30f46b0ef
Tyna Nguyen <ngtyna@gmail.com>
2020-10-12 16:08:38 -0500

    Implement a ForagerBee subclass to inherit properties from Bee (ES6)

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,26 @@
-class ForagerBee {
-  // TODO..
+/*
+
+ForagerBee = Subclass of Bee (superclass)
+Implement in ES6 style (use class, extends, super keywords)
+
+*/
+
+class ForagerBee extends Bee {
+  constructor() {
+    // Calls the Bee superclass
+    super();
+
+    this.age = 10;
+    this.color = 'yellow';
+    this.job = 'find pollen';
+
+    // Add new ForagerBee properties
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  // Add new ForagerBee methods
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
