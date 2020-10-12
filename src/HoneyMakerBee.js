374a2f6c4df6035f9b847ec29a6de1c689fae666
Tyna Nguyen <ngtyna@gmail.com>
2020-10-12 12:36:09 -0500

    Implement a HoneyMakerBee subclass to inherit properties from Bee (ES6)

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,31 @@
-class HoneyMakerBee {
-  // TODO..
+/*
+
+HoneyMakerBee = Subclass of Bee (superclass)
+Implement in ES6 style (use class, extends, super keywords)
+
+*/
+
+class HoneyMakerBee extends Bee {
+  constructor() {
+    // Override the constructor, pass args for base class
+    super();
+
+    this.age = 10;
+    this.job = 'make honey';
+    this.color = 'yellow';
+
+    // Add new HoneyMakerBee properties
+    this.honeyPot = 0;
+  }
+
+  // Add new HoneyMakerBee methods
+  makeHoney() {
+    // Increment honeyPot value by 1
+    this.honeyPot++;
+  }
+
+  giveHoney() {
+    // Decreemnt honeyPot value by 1
+    this.honeyPot--;
+  }
 };
