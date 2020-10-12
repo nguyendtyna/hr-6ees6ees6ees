a5b0f7f1e628219d647bde2bd9ed28c013773011
Tyna Nguyen <ngtyna@gmail.com>
2020-10-12 12:23:03 -0500

    Implement a Grub class in ES6

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,20 @@
+/*
+
+Grub = Parent/superclass to all bees
+Implement in ES6 style (use class, extends, super keywords)
+
+*/
+
 class Grub {
+  // Define a constructor function containing properties
+  constructor() {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+
+  // Define methods
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
 };
